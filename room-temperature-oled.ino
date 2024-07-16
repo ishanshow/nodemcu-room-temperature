43cff3bcf29858871bda75831121199110909922
Ishan Show <105852558+ishanshow@users.noreply.github.com>
2024-07-17 00:38:57 +0530

    Update room-temperature-oled.ino

--- room-temperature-oled.ino
+++ room-temperature-oled.ino
@@ -25,11 +25,7 @@ const char* ssid = "";
 const char* password = "";
 
 #define DHTPIN 0     // Digital pin D1 connected to the DHT sensor
-
-// Uncomment the type of sensor in use:
-//#define DHTTYPE    DHT11     // DHT 11
 #define DHTTYPE    DHT22     // DHT 22 (AM2302)
-//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
 DHT dht(DHTPIN, DHTTYPE);
 
